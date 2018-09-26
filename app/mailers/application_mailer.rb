35ea3d64ba4f31a04664d96a8b82818a39a96714
granbahamut <ivan.dario.pinillagmail.com>
2018-09-25 19:58:03 -0500

    Commit inicial del ejemplo de la tienda en GIT, para subirlo a Heroku

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'ivan.dario.pinilla@gmail.com'
+  layout 'mailer'
+end
---
+++ app/mailers/usermailer_mailer.rb
@@ -0,0 +1,7 @@
+class UsermailerMailer < ApplicationMailer
+    def welcome_email
+        @user = params[:user]
+        @url = 'http://localhost:3000/sign_in'
+        mail(to: @user.email, subject: 'Welcome to zombo.Com!')
+    end
+end
